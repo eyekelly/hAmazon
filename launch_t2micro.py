5f52f149928ff4df2a399d9bca3a64b5506b23a4
Ian Kelly <admin@iksound.co.uk>
2017-10-24 15:35:08 +0100

    output to file

--- launch_t2micro.py
+++ launch_t2micro.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import pdb
+import json
 import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(ImageId='ami-6b7c6e0f',
@@ -9,4 +9,11 @@ instance = ec2.create_instances(ImageId='ami-6b7c6e0f',
     InstanceType='t2.micro',
     SubnetId='subnet-f3d45bbe',
     SecurityGroupIds=['sg-1113f179'])
-print "created instance with id" + " " + instance[0].id
+instance_id = instance[0].id
+print "created instance with id" + " " + instance_id
+
+client = boto3.client('ec2')
+response = client.get_console_output(InstanceId=instance_id)
+
+with open('log.json', 'w') as outfile:
+    json.dump(str(response), outfile)
