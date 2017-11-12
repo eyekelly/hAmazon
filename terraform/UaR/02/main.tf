9d2a081f529e6bff3ae3db116829ffcffa60ab97
Ian Kelly <admin@iksound.co.uk>
2017-11-12 09:55:04 +0000

    adding variables and output to main.tf

--- terraform/UaR/02/main.tf
+++ terraform/UaR/02/main.tf
@@ -1,11 +1,49 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
+}
+
+variable "server_port" {
+  description = "port for http requests"
+  default = 8080
+}
+
+output "public IP" {
+  value = "${aws_instance.UaR_02_1.public_ip}"
+}
+
+resource "aws_security_group" "ssh_access_TCH" {
+  name = "ssh"
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["194.74.174.162/32"]
+  }
+}
+
+resource "aws_security_group" "SG_UaR_02_1" {
+  name = "SG_UaR_02_1"
+  ingress {
+    from_port = "${var.server_port}"
+    to_port   = "${var.server_port}"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "UaR_02_1" {
-  ami = "ami-6b7c6e0f"
+  ami = "ami-40d28157"
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["${aws_security_group.SG_UaR_02_1.id}","${aws_security_group.ssh_access_TCH.id}"]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "hello World" > index.html
+  nohup busybox httpd -f -p "${var.server_port}" &
+  EOF
+
   tags {
-    name = "UaR_02_1"
+    Name = "UaR_02_1"
   }
 }
